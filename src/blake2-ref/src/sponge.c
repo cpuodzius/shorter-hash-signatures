c28250110d38baea39aab30589d4b557cc00379c
geovandro <geovandro.pereira@gmail.com>
2014-01-14 14:13:11 -0200

    adding sponge.c

---
+++ src/blake2-ref/src/sponge.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "../include/sponge.h"
+
+void sinit(sponge_t *sponge, short seclevel) {
+    if (seclevel > (BLAKE2B_OUTBYTES << 3)) {
+        seclevel = (BLAKE2B_OUTBYTES << 3); // maximum
+    }
+    if (sponge != NULL) {
+        blake2s_init(sponge, seclevel >> 3);
+    }
+}
+
+void absorb(sponge_t *sponge, const void *data, short len) {
+    if (sponge != NULL) {
+        blake2s_update(sponge, (uint8_t *)data, len);
+    }
+}
+
+void squeeze(sponge_t *sponge, void *digest, short len) {
+    if (sponge != NULL) {
+        blake2s_final(sponge, digest, len);
+    }
+}
+
+void cleanup(sponge_t *sponge) {
+    if (sponge != NULL) {
+        memset(sponge, 0, sizeof(sponge_t));
+    }
+}
