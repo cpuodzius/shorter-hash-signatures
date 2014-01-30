831104b458a2ff5d4213e33b63fedff63dbd2325
Cassius Puodzius <puodzius@gmail.com>
2014-01-30 19:46:54 -0200

    Ficou faltando incluir winternitz.h no commit passado

--- include/winternitz.h
+++ include/winternitz.h
@@ -12,44 +12,45 @@ extern "C" {
 #define OK 0
 #define ERROR 1
 
-// w: word size in bits
-// sec level = n - w - 1 - 2log_2(l*w) (security for pre-image resistance)
+// WINTERNITZ_W: word size in bits
+// WINTERNITZ_SEC_LVL = WINTERNITZ_N - WINTERNITZ_W - 1 - 2log_2(WINTERNITZ_L*WINTERNITZ_W) (security for pre-image resistance)
 
 // sec level: 82
-#define SEC (82)
-#define W (8)
-#define n (104)
+#define WINTERNITZ_SEC_LVL (82)
+#define WINTERNITZ_W (8)
+#define WINTERNITZ_N (104)
 
 // sec level: 86
-//#define SEC (86)
-//#define W (4)
-//#define n (104)
+//#define WINTERNITZ_SEC (86)
+//#define WINTERNITZ_W (4)
+//#define WINTERNITZ_N (104)
 
 // sec level: 97
 //#define W_BITS (8)
-//#define n (120)
+//#define WINTERNITZ_N (120)
 
 // sec level: 101
-//#define W (4)
-//#define n (120)
+//#define WINTERNITZ_W (4)
+//#define WINTERNITZ_N (120)
 
 // sec level: 129
-//#define W (8)
-//#define n (152)
+//#define WINTERNITZ_W (8)
+//#define WINTERNITZ_N (152)
 
 // sec level: 126
-//#define W (4)
-//#define n (144)
+//#define WINTERNITZ_W (4)
+//#define WINTERNITZ_N (144)
 
-#define TwotoW (1 << W)
-#define l1 ( (n + W - 1) / W)
+#define TwotoW (1 << WINTERNITZ_W)
 //TODO: Find the smallest possible checksum size
-#define CSSize 2
-#define l2 (CSSize)
-#define l (l1 + CSSize)
-#define L (l1 + CSSize)
+#define WINTERNITZ_CHECKSUM_SIZE 2
+#define WINTERNITZ_l1 ( (WINTERNITZ_N + WINTERNITZ_W - 1) / WINTERNITZ_W)
+#define WINTERNITZ_l2 (WINTERNITZ_CHECKSUM_SIZE)
+#define WINTERNITZ_l (WINTERNITZ_l1 + WINTERNITZ_CHECKSUM_SIZE)
+#define WINTERNITZ_L (WINTERNITZ_l1 + WINTERNITZ_CHECKSUM_SIZE)
 
-#define GetWBits(x, startbit) ((x & (unsigned)( (unsigned)((1 << W) - 1) << startbit)) >> startbit)
+#define GetWBits(x, startbit) ((x & (unsigned)( (unsigned)((1 << WINTERNITZ_W) - 1) << startbit)) >> startbit)
+#define LEN_BYTES(len_bits) ((len_bits+7)/8)
 
 typedef sponge_t oneway_state;
 typedef unsigned char rand_dig_f_type(void);
@@ -60,11 +61,11 @@ short Rand(unsigned char *x, short bits, rand_dig_f_type rand_dig_f);
 void onewayfunc(oneway_state *f, unsigned char *input, short inputlen, unsigned char *output, short outputlen);
 void onewayfunc_mult(oneway_state *f, short iter, unsigned char *input, short inputlen, unsigned char *output, short outputlen);
 
-void wots_getprivkey(oneway_state *f, unsigned char s[L][l1], unsigned char *seed, short seedd);
-void wots_keygen(oneway_state *f, unsigned char s[L][l1], unsigned char v[L][l1], unsigned char V[l1], unsigned char *seed, short seedd);
-void wots_computechecksum(unsigned char M[L]);
-unsigned char wots_sign(oneway_state *f, unsigned char M[L], unsigned char s[L][l1], unsigned char S[L][l1]);
-unsigned char wots_verify(oneway_state *f, unsigned char m[L], unsigned char V[l1], unsigned char S[L][l1]);
+void wots_getprivkey(oneway_state *f, unsigned char s[WINTERNITZ_L][WINTERNITZ_l1], unsigned char *seed, short seedd);
+void wots_keygen(oneway_state *f, unsigned char s[WINTERNITZ_L][WINTERNITZ_l1], unsigned char v[WINTERNITZ_L][WINTERNITZ_l1], unsigned char V[WINTERNITZ_l1], unsigned char *seed, short seedd);
+void wots_computechecksum(unsigned char M[WINTERNITZ_L]);
+unsigned char wots_sign(oneway_state *f, unsigned char M[WINTERNITZ_L], unsigned char s[WINTERNITZ_L][WINTERNITZ_l1], unsigned char S[WINTERNITZ_L][WINTERNITZ_l1]);
+unsigned char wots_verify(oneway_state *f, unsigned char m[WINTERNITZ_L], unsigned char V[WINTERNITZ_l1], unsigned char S[WINTERNITZ_L][WINTERNITZ_l1]);
 
 #ifdef __cplusplus
 };
